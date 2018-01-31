cdde1bac8cd8484f6d5b5b60225cd052562f9938
fdlessard <fdlessard@gmail.com>
2018-01-30 21:42:33 -0500

    Added vault configs

--- src/main/java/io/fdlessard/codebites/vault/VaultApplication.java
+++ src/main/java/io/fdlessard/codebites/vault/VaultApplication.java
@@ -1,12 +1,25 @@
 package io.fdlessard.codebites.vault;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class VaultApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VaultApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VaultApplication.class, args);
+    }
+
+
+    @Value("${password}")
+    String password;
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("My password is: " + password);
+    }
+
 }
